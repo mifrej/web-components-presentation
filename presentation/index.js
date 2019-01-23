df3d43e37ae631c24054b3820b64aee9df89c5ee
Milosz Frejnik <milosz@frejnik.com>
2019-01-23 17:21:03 +0100

    Draft of presentation

--- presentation/index.js
+++ presentation/index.js
@@ -33,7 +33,9 @@ import './assets/cobalt2.prism.css'
 import preloader from "spectacle/lib/utils/preloader";
 
 const images = {
-  jsState: require('../assets/dependencies_js.jpg')
+  customElement: require('../assets/custom_element.jpg'),
+  htmlTemplates: require('../assets/html_templates.jpg'),
+  shadowDom: require('../assets/shadow_dom.jpg'),
 }
 
 preloader(images);
@@ -74,190 +76,51 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
+
         <Slide transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            ES6 essentials
+            Web Components
           </Heading>
         </Slide>
+
         <Slide transition={["fade"]} bgColor="tertiary">
-          <HeadingStyled size={2} textColor="secondary">Goal 🥅</HeadingStyled>
+          <HeadingStyled size={1} textColor="secondary">What Web Components mean? 🤷🏻‍♂️</HeadingStyled>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary">
+          <HeadingStyled size={2} textColor="primary">Web components are a set of web platform APIs that allow you to create new custom, reusable, encapsulated HTML tags to use in web pages and web apps.</HeadingStyled>
+          <HeadingStyled size={2} textColor="tertiary">Specifications</HeadingStyled>
           <CustomList>
-            <StyledListItem>Get to know essential syntax in order to get productive.</StyledListItem>
-            <StyledListItem>Recognize common baseline in the team</StyledListItem>
-            <StyledListItem>Introduction to new Frontend architecture</StyledListItem>
+            <StyledListItem>Custom Element</StyledListItem>
+            <StyledListItem>HTML template</StyledListItem>
+            <StyledListItem>Shadow DOM</StyledListItem>
           </CustomList>
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <HeadingStyled size={4} textColor="secondary" caps>Boilerplate Code</HeadingStyled>
-          <HeadingStyled size={6} textColor="secondary">
-            <Link href="https://github.com/mifrej/es6-essentials" target="_blank" > https://github.com/mifrej/es6-essentials</Link>
-          </HeadingStyled>
-          <Text>We will be coding it in Code Sandbox</Text>
-        </Slide>
+
         <Slide>
-          <HeadingStyled size={2}>ES Modules</HeadingStyled>
-          <Text>Exports</Text>
-          <CodePane lang="js" source={require('raw-loader!./assets/modules_export')} textSize="22" theme="external" contentEditable />
+          <HeadingStyled size={2}>Custom Element</HeadingStyled>
+          <Text>The Custom Elements specification lays the foundation for designing and using new types of DOM elements.</Text>
+          <CodePane lang="js" source={require('raw-loader!./assets/custom_element')} textSize="22" theme="external" contentEditable />
         </Slide>
+
         <Slide>
-          <HeadingStyled size={2}>ES Modules</HeadingStyled>
-          <Text>Imports</Text>
-          <CodePane lang="js" source={require('raw-loader!./assets/modules_import')} textSize="22" theme="external" contentEditable />
+          <HeadingStyled size={2}>HTML template</HeadingStyled>
+          <Text>The HTML template element specification defines how to declare fragments of markup that go unused at page load, but can be instantiated later on at runtime.</Text>
+          <CodePane lang="js" source={require('raw-loader!./assets/html_template')} textSize="22" theme="external" contentEditable />
         </Slide>
-        <Slide bgColor="tertiary">
-          <HeadingStyled size={2}>Exercise</HeadingStyled>
-          <HeadingStyled size={6} textAlign="left">In <Code>src/es6/starter/modules.js</Code></HeadingStyled>
-          <CustomList ordered>
-            <StyledListItem>Setup a function as <Code>default export</Code></StyledListItem>
-            <StyledListItem>Setup at least 2 <Code>named exports</Code></StyledListItem>
-          </CustomList>
-          <HeadingStyled size={6} textAlign="left">In <Code>src/index.js</Code></HeadingStyled>
-          <CustomList ordered>
-            <StyledListItem>Import <Code>default export</Code> and execute it</StyledListItem>
-            <StyledListItem>Import <Code>named exports</Code> and console log them</StyledListItem>
-          </CustomList>
-        </Slide>
-        <Slide transition={["fade"]}>
-          <HeadingStyled size={2} textColor="secondary">Variables</HeadingStyled>
-          <HeadingStyled size={4} textColor="secondary">Hoisting</HeadingStyled>
-          <CodePane lang="js" source={require('raw-loader!./assets/vars_hoisting')} textSize="22" theme="external" contentEditable />
-        </Slide>
-        <Slide transition={["fade"]}>
-          <HeadingStyled size={2} textColor="secondary">Variables</HeadingStyled>
-          <HeadingStyled size={4}>New types - <CodeStyled>const</CodeStyled> and <CodeStyled>let</CodeStyled></HeadingStyled>
-          <CodePane lang="js" source={require('raw-loader!./assets/vars')} textSize="22" theme="external" contentEditable />
-        </Slide>
+
         <Slide>
-          <HeadingStyled size={3} textColor="secondary"><CodeStyled>const, let</CodeStyled> characteristics</HeadingStyled>
-          <CustomList>
-            <StyledListItem>Both are block scoped <Code>{`{ let foo = 5 }`}</Code></StyledListItem>
-            <StyledListItem>Cannot be redeclared</StyledListItem>
-            <StyledListItem><Code>const</Code> is read only (although you may add and change object properties)</StyledListItem>
-            <StyledListItem>They are not hoisted by the interpreter</StyledListItem>
-          </CustomList>
-        </Slide>
-        <Slide>
-          <HeadingStyled size={2}>Exercise</HeadingStyled>
-          <CustomList ordered>
-            <StyledListItem>
-              In file <Code>src/es6/starter/variables.js</Code>,
-              convert <Code>var me</Code> to a <Code>const</Code> type.<br />
-            </StyledListItem>
-            <StyledListItem>Add <Code>surname</Code> prop to the object in <Code>const me</Code></StyledListItem>
-            <StyledListItem>Fix the for loop to console log consequent iteration number</StyledListItem>
-          </CustomList>
-        </Slide>
-        <Slide>
-          <HeadingStyled size={4}>Template strings</HeadingStyled>
-          <CodePane lang="js" source={require('raw-loader!./assets/template_strings')} textSize="22" theme="external" contentEditable />
+          <HeadingStyled size={2}>Shadow DOM</HeadingStyled>
+          <Text>The shadow DOM specification defines how to use encapsulated style and markup in web components.</Text>
+          <CodePane lang="js" source={require('raw-loader!./assets/shadow_dom')} textSize="22" theme="external" contentEditable />
         </Slide>
 
         <Slide bgColor="tertiary">
-          <HeadingStyled size={2}>Exercise</HeadingStyled>
-          <CustomList ordered>
-            <StyledListItem>Rewrite ES5 string concatenation, into template string  <Code>src/es6/starter/template_strings.js</Code></StyledListItem>
-            <StyledListItem>List members names with consequent numbers. Each member name should be introduced in new line, like (<Code>Member nr 1 is Alexander</Code>)</StyledListItem>
-          </CustomList>
+          <HeadingStyled size={2}>Browsers support</HeadingStyled>
+          <Image src={images.customElement} />
+          <Image src={images.htmlTemplates} />
+          <Image src={images.shadowDom} />
         </Slide>
-
-        <Slide>
-          <HeadingStyled size={4}>Tagged templates</HeadingStyled>
-          <CodePane lang="js" source={require('raw-loader!./assets/tagged_templates')} textSize="22" theme="external" contentEditable />
-        </Slide>
-
-        <Slide>
-          <HeadingStyled size={4}>Browser native DOM selector methods</HeadingStyled>
-          <Text textColor="secondary">Where is my <Code>$</Code> selector 😲</Text>
-          <Image src="https://media.giphy.com/media/3o84U72tKO389H2lI4/giphy.gif"/>
-        </Slide>
-
-        <Slide>
-          <HeadingStyled size={4}>JS state today 😱</HeadingStyled>
-          <Image src={images.jsState} />
-        </Slide>
-
-        <Slide>
-          <HeadingStyled size={4}>Native browser <CodeStyled>document</CodeStyled> object methods for DOM manipulations</HeadingStyled>
-          <CustomList>
-            <StyledListItem><Code>getElementById('id_of_element')</Code></StyledListItem>
-            <StyledListItem><Code>querySelector('any_selector')</Code></StyledListItem>
-            <StyledListItem><Code>querySelectorAll('any_selector')</Code></StyledListItem>
-            <StyledListItem><Code>appendChild(node)</Code></StyledListItem>
-            <StyledListItem><Code>append((Node or DOMString)... nodes)</Code></StyledListItem>
-            <StyledListItem><Code>createElement('tagName')</Code></StyledListItem>
-          </CustomList>
-        </Slide>
-        <Slide bgColor="tertiary">
-          <HeadingStyled size={2}>Exercise</HeadingStyled>
-          <CustomList ordered>
-            <StyledListItem>Append provided ul list to a document</StyledListItem>
-            <StyledListItem>Select all nodes with <Code>[data-time]</Code> attribute</StyledListItem>
-            <StyledListItem>Select all nodes with classname <Code>special-element</Code></StyledListItem>
-            <StyledListItem>Create new <Code>ul</Code> list with one <Code>li</Code></StyledListItem>
-            <StyledListItem>Append new created <Code>ul</Code> list to a DOM</StyledListItem>
-          </CustomList>
-        </Slide>
-        <Slide>
-          <HeadingStyled size={3}>Function declaration</HeadingStyled>
-          <HeadingStyled size={5}>With Hoisting</HeadingStyled>
-          <CodePane lang="js" source={require('raw-loader!./assets/function_hoisting')} textSize="22" theme="external" contentEditable />
-        </Slide>
-        <Slide>
-          <HeadingStyled size={3}>Function expression</HeadingStyled>
-          <CodePane lang="js" source={require('raw-loader!./assets/function_expression')} textSize="22" theme="external" contentEditable />
-        </Slide>
-        <Slide>
-          <HeadingStyled size={3}>Arrow <CodeStyled>=></CodeStyled> function</HeadingStyled>
-          <CodePane lang="js" source={require('raw-loader!./assets/arrow_function')} textSize="22" theme="external" contentEditable />
-        </Slide>
-        <Slide>
-          <HeadingStyled size={3}><CodeStyled>=></CodeStyled> Function characteristics</HeadingStyled>
-          <CustomList>
-            <StyledListItem>Does not change <Code>this</Code> context</StyledListItem>
-            <StyledListItem>Doesn't have keyword <Code>arguments</Code></StyledListItem>
-            <StyledListItem>Does not replace regular function declaration</StyledListItem>
-            <StyledListItem>Reduce code boilerplate when anonymous function required</StyledListItem>
-          </CustomList>
-        </Slide>
-        <Slide bgColor="tertiary">
-          <HeadingStyled size={2}>Exercise</HeadingStyled>
-          <CustomList ordered>
-            <StyledListItem>Rewrite ES5 functions to arrow functions from <Code>src/es6/starter/arrow_function.js</Code></StyledListItem>
-            <StyledListItem>Fix script from <Code>src/es6/starter/arrow_function_this.js</Code></StyledListItem>
-          </CustomList>
-        </Slide>
-        <Slide>
-          <HeadingStyled size={2}>Default function arguments</HeadingStyled>
-          <CodePane lang="js" source={require('raw-loader!./assets/function_default_arguments')} textSize="22" theme="external" contentEditable />
-        </Slide>
-        <Slide>
-          <HeadingStyled size={2}>Object literal improvements</HeadingStyled>
-          <CodePane lang="js" source={require('raw-loader!./assets/object_literal_improvements')} textSize="22" theme="external" contentEditable />
-        </Slide>
-        <Slide bgColor="tertiary">
-          <HeadingStyled size={2}>Exercise</HeadingStyled>
-          <HeadingStyled size={6} textAlign="left">In <Code>src/es6/starter/object_literal.js</Code></HeadingStyled>
-          <CustomList ordered>
-            <StyledListItem>Improve the syntax of the object with ES6 upgrades</StyledListItem>
-            <StyledListItem>Make the <Code>cycle</Code> prop name being dynamic from <Code>const activity</Code></StyledListItem>
-          </CustomList>
-        </Slide>
-
-        <Slide>
-          <HeadingStyled size={2}>Destructuring</HeadingStyled>
-          <CodePane lang="js" source={require('raw-loader!./assets/destructuring')} textSize="22" theme="external" contentEditable />
-        </Slide>
-
-        <Slide bgColor="tertiary">
-          <HeadingStyled size={2}>Exercise</HeadingStyled>
-          <HeadingStyled size={6} textAlign="left">In <Code>src/es6/starter/destructuring.js</Code></HeadingStyled>
-          <CustomList ordered>
-            <StyledListItem>Destructure properties <Code>first, last, twitter</Code> from <Code>person</Code> object</StyledListItem>
-            <StyledListItem>Destructure properties <Code>twitter, facebook</Code> with aliasing them to <Code>tweet, fb</Code> from object <Code>me</Code></StyledListItem>
-            <StyledListItem>Destructure <Code>width, color</Code> from <Code>settings</Code> with default values <Code>200, 'yellow'</Code></StyledListItem>
-          </CustomList>
-        </Slide>
-
       </Deck>
     );
   }
